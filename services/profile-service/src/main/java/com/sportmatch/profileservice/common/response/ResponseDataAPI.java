eb8296ff55aaba3a9e1ba801b7619f49a1623519
Văn Công Chí Thanh <vancongchithanh2004@gmail.com>
2025-12-30 09:56:31 +0700

    fix: import package

--- services/profile-service/src/main/java/com/sportmatch/profileservice/common/response/ResponseDataAPI.java
+++ services/profile-service/src/main/java/com/sportmatch/profileservice/common/response/ResponseDataAPI.java
@@ -1,7 +1,7 @@
 package com.sportmatch.profileservice.common.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.sportmatch.identityservice.common.constant.CommonConstant;
+import com.sportmatch.profileservice.common.constant.CommonConstant;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
